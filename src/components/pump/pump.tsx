9529cc1a66f821d04b6860aa5b44cd99a45dffb4
Alexander Bolotskov <r3disrussian@gmail.com>
2021-09-28 23:11:28 +0800

    feat(pump): 🎸 adds value increasing

--- src/components/pump/pump.tsx
+++ src/components/pump/pump.tsx
@@ -4,10 +4,13 @@ import './pump.css'
 export interface PumpProps {
 	value?: number
 	step?: number
+	max?: number
 }
 
 export interface PumpState {
 	isPumping: boolean
+	isValueIncreasable: boolean
+	value: number
 }
 
 export class Pump extends React.Component<PumpProps, PumpState> {
@@ -17,43 +20,91 @@ export class Pump extends React.Component<PumpProps, PumpState> {
 		}
 	}
 
+	private _mouseMoveListener = (event: MouseEvent) => {
+		if (this.state.isPumping) {
+			this._movePiston(event)
+		}
+	}
+
+	private _pistonRef = React.createRef<HTMLDivElement>();
+
 	constructor(props: PumpProps) {
 		super(props)
 		this.state = {
 			isPumping: false,
+			value: this.props.value ?? 0,
+			isValueIncreasable: true
 		}
 	}
 
 	componentDidMount(): void {
 		window.addEventListener('mouseup', this.mouseUpListener.bind(this))
+		window.addEventListener('mousemove', this._mouseMoveListener.bind(this))
 	}
 
 	componentWillUnmount(): void {
 		window.removeEventListener('mouseup', this.mouseUpListener.bind(this))
+		window.removeEventListener('mousemove', this._mouseMoveListener.bind(this))
 	}
 
 	render() {
-		const value = this.props.value ?? 0
 		return (<div className="wrap">
-			<div className="piston">
+			<div className="piston" ref={this._pistonRef}>
 				<span
 					className="piston__handle"
 					onMouseDown={event => this._startPumping(event)}
 					onMouseUp={event => this._endPumping(event)}
-					onMouseMove={event => this.state.isPumping && this._movePiston(event)}
 				></span>
 			</div>
 			<div className="cylinder">
-				<span className="caption">{value}</span>
+				<span className="caption">{this.state.value}</span>
 				<span className="progress-bar">
-					<span className="progress-bar__filling" style={{ height: `${value}%` }}></span>
+					<span className="progress-bar__filling" style={{ height: `${this.state.value}%` }}></span>
 				</span>
 			</div>
 		</div>)
 	}
 
-	private _movePiston(event: React.MouseEvent<HTMLSpanElement, MouseEvent>): void {
-		console.warn('Why?')
+	private _movePiston(event: MouseEvent): void {
+		const piston = this._pistonRef.current;
+		if (piston === null) {
+			return;
+		}
+		let currentPosition = parseInt(piston.style.top, 10)
+		if (isNaN(currentPosition)) {
+			currentPosition = 0
+		}
+		const { movementY } = event
+		if ((movementY < 0 && currentPosition <= 0) || (movementY > 0 && currentPosition >= 100)) {
+			return
+		}
+		const updatedValue = currentPosition + movementY
+		const valueToSet = updatedValue > 100 ? 100 : updatedValue < 0 ? 0 : updatedValue
+		piston.style.top = `${valueToSet}px`
+
+		if (valueToSet > 90 && this.state.isValueIncreasable) {
+			this._increaseValue()
+			this.setState({
+				isValueIncreasable: false
+			})
+		} else if (valueToSet < 10) {
+			this.setState({
+				isValueIncreasable: true
+			})
+		} 
+	}
+
+	private _increaseValue(): void {
+		const step = this.props.step ?? 5
+		const maxValue = this.props.max ?? 100
+		const updateValue = this.state.value + step
+		const valueToSet = updateValue > maxValue ? maxValue : updateValue < 0 ? 0 : updateValue
+
+		if (this.state.value <= maxValue) {
+			this.setState({
+				value: valueToSet
+			})
+		}
 	}
 
 	private _startPumping(event: React.MouseEvent<HTMLSpanElement, MouseEvent>): void {
@@ -69,6 +120,11 @@ export class Pump extends React.Component<PumpProps, PumpState> {
 		this.setState({
 			isPumping: false
 		})
+		const piston = this._pistonRef.current;
+		if (piston === null) {
+			return
+		}
+		piston.style.top = '';
 		console.debug('_endPumping')
 	}
 }
