2752bf2de8d9944902d05f3f1511793ce30aec10
Alexander Bolotskov <r3disrussian@gmail.com>
2021-09-28 23:43:29 +0800

    refactor: 💡 adds proper components grid

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,16 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<figure>
-					<Pump value={0} />
-					<figcaption>Pump</figcaption>
-				</figure>
+				<h1>It's painful to use</h1>
+				<div className="grid">
+					<figure className="component">
+						<Pump value={0} />
+						<figcaption>Pump</figcaption>
+						<pre>
+							<code>&lt;Pump value={0} /&gt;</code>
+						</pre>
+					</figure>
+				</div>
 			</div>
 		);
 	}
